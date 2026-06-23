3ae949c2497786937459728c5bb45f72ab10d365
Bertrand Tang <bertand.tang.dev@gmail.com>
2026-06-23 14:45:18 +0200

    activité 10

---
+++ app/controllers/wood.js
@@ -0,0 +1,3 @@
+export const getAllWoods = (req, res) => {
+    res.send("List of woods");
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import userRoutes from "./user.js";
 import woodRoutes from "./wood.js";
 
-const router = express();
+const router = express.Router();
 
 router.use("/auth", userRoutes);
 router.use("/woods", woodRoutes);
