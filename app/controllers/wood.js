63fa41150caeef5ca93d181a7a92c328c30caa46
Bertrand Tang <bertand.tang.dev@gmail.com>
2026-06-23 16:56:15 +0200

    activité 14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,14 @@
-export const getAll = (req, res) => {
-    res.send("List of woods");
+import { prisma } from "../../app.js"; 
+
+export const getAll = async (req, res) => {
+    try {
+        const woods = await prisma.wood.findMany();
+
+        res.status(200).json(woods);
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message || "An error occurred while fetching the woods.",
+        });
+    }
 };
