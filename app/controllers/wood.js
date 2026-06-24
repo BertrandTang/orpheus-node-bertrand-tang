aac7d3acd13eeef0c1acd086df0e505910d0d534
Bertrand Tang <bertand.tang.dev@gmail.com>
2026-06-24 10:31:15 +0200

    activité 15

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,4 +1,4 @@
-import { prisma } from "../../app.js"; 
+import { prisma } from "../../app.js";
 
 export const getAll = async (req, res) => {
     try {
@@ -7,6 +7,22 @@ export const getAll = async (req, res) => {
         res.status(200).json(woods);
     } catch (error) {
 
+        res.status(500).json({
+            error: error.message || "An error occurred while fetching the woods.",
+        });
+    }
+};
+
+export const readByHardness = async (req, res) => {
+    try {
+        const hardness = req.params.hardness;
+
+        const woods = await prisma.wood.findMany({
+            where: { hardness: hardness },
+        });
+
+        res.status(200).json(woods);
+    } catch (error) {
         res.status(500).json({
             error: error.message || "An error occurred while fetching the woods.",
         });
