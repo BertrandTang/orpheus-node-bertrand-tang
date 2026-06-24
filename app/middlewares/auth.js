c22a08f4d9acb8d5e8117c71c74397a2e202f8d8
Bertrand Tang <bertand.tang.dev@gmail.com>
2026-06-24 14:50:13 +0200

    activté 17

---
+++ app/middlewares/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            return res.status(401).json({ error: 'Missing token' });
+        }
+
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
+        const userId = decodedToken.id;
+        req.auth = { userId };
+        next();
+    } catch (err) {
+        res.status(401).json({
+            error: 'Unauthorized Token'
+        });
+    }
+};
